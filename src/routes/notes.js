e43c776a47f98865a832a489378ecaf13efeed0b
Marco Alfredo Hernández López <mahernandez@minex.gob.gt>
2021-07-01 15:32:27 -0600

    Agregar Note

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 
-router.get('/notes', (req, res)=>{
+router.get('/notes/add', (req,res) => {
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-notes', (req, res) => {
+    console.log(req.body);
+    res.send('Ok');
+});
+
+router.get('/notes', (req, res) => {
     res.send('Notes desde la base de datos');
 });
 
